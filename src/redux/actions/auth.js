ee594a5aaf61fc9127d21cde1054ef32d97dde61
uijin <justwaves@naver.com>
2020-03-10 23:58:19 +0900

    .

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,43 +1,37 @@
-import caver from 'klaytn/caver'
-import {
-  LOGIN,
-  LOGOUT,
-  INTEGRATE_WALLET,
-  REMOVE_WALLET,
-} from './actionTypes'
+import caver from "klaytn/caver";
+import { LOGIN, LOGOUT, INTEGRATE_WALLET, REMOVE_WALLET } from "./actionTypes";
 
-export const integrateWallet = (privateKey) => (dispatch) => {
-  const walletInstance = caver.klay.accounts.privateKeyToAccount(privateKey)
-  caver.klay.accounts.wallet.add(walletInstance)
-  sessionStorage.setItem('walletInstance', JSON.stringify(walletInstance))
+export const integrateWallet = privateKey => dispatch => {
+  const walletInstance = caver.klay.accounts.privateKeyToAccount(privateKey);
+  caver.klay.accounts.wallet.add(walletInstance);
+  sessionStorage.setItem("walletInstance", JSON.stringify(walletInstance));
   return dispatch({
     type: INTEGRATE_WALLET,
     payload: {
       privateKey,
-      address: walletInstance.address,
-    },
-  })
-}
+      address: walletInstance.address
+    }
+  });
+};
 
-export const removeWallet = () => (dispatch) => {
-  caver.klay.accounts.wallet.clear()
-  sessionStorage.removeItem('walletInstance')
+export const removeWallet = () => dispatch => {
+  caver.klay.accounts.wallet.clear();
+  sessionStorage.removeItem("walletInstance");
   return dispatch({
-    type: REMOVE_WALLET,
-  })
-}
+    type: REMOVE_WALLET
+  });
+};
 
-export const login = (privateKey) => (dispatch) => {
-  dispatch(integrateWallet(privateKey))
+export const login = privateKey => dispatch => {
+  dispatch(integrateWallet(privateKey));
   return dispatch({
-    type: LOGIN,
-  })
-}
+    type: LOGIN
+  });
+};
 
-export const logout = () => (dispatch) => {
-  dispatch(removeWallet())
+export const logout = () => dispatch => {
+  dispatch(removeWallet());
   return dispatch({
-    type: LOGOUT,
-  })
-}
-
+    type: LOGOUT
+  });
+};
